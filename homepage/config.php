4b91abe3f33ecfedb4cfb0b0c8c21664b24afdb0
Sung Woo Oh <sungbr95@gmail.com>
2020-12-08 09:05:03 -0500

    db connection & some path changes to psu server url

--- homepage/config.php
+++ homepage/config.php
@@ -1,4 +1,9 @@
 <?php
+  $servername = "CMPSC431-S3-G-14.vmhost.psu.edu";
+  $mysql_username = "431group";
+  $mysql_password = "password";
+  $db = "431group_db";
+  
   if(!isset($_SESSION)) { 
     session_start(); 
   } 
@@ -6,11 +11,6 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-
-  $db = "431group_db";
-  $servername = "CMPSC431-S3-G-14.vmhost.psu.edu";
-  $mysql_username = "431group";
-  $mysql_password = "password";
 
   try {
   $con = mysqli_connect($servername, $mysql_username, $mysql_password);
