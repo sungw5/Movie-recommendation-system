4b29a9c3057b9490ce0f99fc87559a642e10bb85
Mingda <74105734+CMD1020@users.noreply.github.com>
2020-12-08 15:39:56 +0800

    Update homepage.php

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -10,6 +10,7 @@ if (is_logged_in() == false) {
 
 $username = $_SESSION['user']['username'];
 
+
 if(isset($_POST['movie_name'])) {
       $movie_name = $_POST['movie_name'];
       $movie_id = $_POST['movie_id'];
@@ -59,13 +60,10 @@ if(isset($_POST['movie_name'])) {
             <a class="nav-link" href="request.php">Request <span class="sr-only">(current)</span></a>
           </li>
         </ul>
-        <a class="nav-link" href="../profile/profile.php">
-          <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-person-circle mr-5" fill="white" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
-            <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-            <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
-          </svg>
-        </a>
+        <form action="../profile/profile.php">
+            <input type="hidden" name="username" value="<?php echo htmlspecialchars($username) ?>"> 
+            <input type="submit" value="Profile">
+          </form> 
         <form class="form-inline my-2 my-lg-0">
           <a class="btn btn-danger my-2 my-sm-0" href="../logout.php" role="button">Logout</a>
         </form>
@@ -74,7 +72,7 @@ if(isset($_POST['movie_name'])) {
     <div class="jumbotron">
       <h1 class="display-4">Welcome, 
         <?php  if (isset($_SESSION['user'])) : ?>
-        <?php echo $_SESSION['user']['name']; ?>
+        <?php echo $_SESSION['user']['username']; ?>
         <div class="text-left">
           <p class="h4">
             <i  style="color: #888;">(logged in as <?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
